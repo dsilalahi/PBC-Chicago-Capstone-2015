36b1745bc8c1f5fd6dcc3bc83a13bdc82693be60
Hamdan Ahmad <hamdan@gmail.com>
2015-11-20 15:17:52 +0000

    changed the sql comment to scala comments

--- spark/src/main/scala/CreditFraudDetection.scala
+++ spark/src/main/scala/CreditFraudDetection.scala
@@ -14,7 +14,7 @@ object CreditFraudDetection {
     val stores =  sc.cassandraTable("retail","stores").select("store_id","state").keyBy(row => row.getString("store_id"))
     val creditcard_by_store = receipts_by_creditcard.join(stores).persist()
 
-    --gets the credit card that were being used fraudulently (use int two different states)
+    //gets the credit card that were being used fraudulently (use int two different states)
     val creditcard_fraud= creditcard_by_store.map{ case (store_id,(result1,result2)) => (result1.getString("credit_card_number"),result2.getString("state"))}.distinct.countByKey().filter{ case (k,v) => v > 1}
     sc.parallelize(creditcard_fraud.toList).saveToCassandra("retail","credit_card_fraud_list",SomeColumns("credit_card_number","num_distinct_state"))
   }
